df7ad9f26b19bfe8b42afafdf930d337a316fc2d
AliceBGott <88068981+AliceBGott@users.noreply.github.com>
2022-07-28 16:39:54 -0300

    feat: Exercício 8

---
+++ exercicio_8.js
@@ -0,0 +1,9 @@
+const Pessoa = {
+  nome: 'Calcifer',
+  idade: '99',
+  getName() => this.nome
+  setName : palavra => this.nome = palavra
+
+  getIdade() => this.getIdade
+  setIdade: numero => this.idade = numero
+}
