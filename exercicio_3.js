f4e15a41d2d8e81aecf8fc62a305aebbeb8333b2
AliceBGott <88068981+AliceBGott@users.noreply.github.com>
2022-07-28 13:11:09 -0300

    feat: Exercício 3

---
+++ exercicio_3.js
@@ -0,0 +1,8 @@
+const listaNomes = ["Edmarques", "Lize", "Fernanda", "Albuquerque", "Breno", "Elisiane",
+                              "Calcifer", "Eric", "Sophie", "Hawl"]
+
+function cumprimento(nome){
+  console.log(`Bem vind@, ${nome}, tenha um ótimo dia!`)
+}
+
+listaNomes.forEach(cumprimento)
